55a458a1900f4878a2a142dcb50f6d0b0c348eca
Kyle Dobitz <kyledobitz@gmail.com>
2014-04-26 11:19:38 -0700

    Added ortho projection example

--- BrainMitten/Assets/BrainMitten/Scripts/StuckInSphere.cs
+++ BrainMitten/Assets/BrainMitten/Scripts/StuckInSphere.cs
@@ -6,15 +6,17 @@ public class StuckInSphere : MonoBehaviour {
 	public float gravityStrength = 10f;
 	public float sphereRadius = 20f;
 	public GameObject center;
-	public float bounceRatio = 0.5f;
+	public float bounceRatio = 0.9f;
 
 	void FixedUpdate () {
 		var centerPosition = center.transform.position;
-		if(Vector3.Distance(centerPosition,  transform.position) > sphereRadius)
+		var upVector = centerPosition - transform.position;
+		if(upVector.magnitude > sphereRadius)
 		{
-			rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, (centerPosition - transform.position).normalized) * bounceRatio;
+			rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, (centerPosition - transform.position).normalized) * 0.9f;
 			//Vector3 f = new Vector3(Random.value, Random.value, Random.value) * 20;
 			//rigidbody.AddForce( f);
 		}
+		rigidbody.AddForce( - upVector.normalized * gravityStrength );
 	}
 }
