a8460c84b8f4fb348ba391e12a75b810d3969e73
kydo986 <kyledobitz@gmail.com>
2014-04-26 01:17:44 -0700

    BrainMitten unity project with the containing sphere proof of concept attempt

---
+++ BrainMitten/Assets/BrainMitten/Scripts/RepulsiveGravity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepulsiveGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider collider){
+		Debug.Log ("On Trigger Stay");
+		collider.gameObject.rigidbody.AddForce (Vector3.forward * 10f);
+	}
+
+	void OnTriggerExit(Collider collider){
+		collider.gameObject.rigidbody.velocity = collider.gameObject.rigidbody.velocity * -0.5f;
+	}
+}
---
+++ BrainMitten/Assets/BrainMitten/Scripts/StuckInSphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuckInSphere : MonoBehaviour {
+
+	public float gravityStrength = 10f;
+	public float sphereRadius = 20f;
+	public GameObject center;
+	public float bounceRatio = 0.5f;
+
+	void FixedUpdate () {
+		var centerPosition = center.transform.position;
+		if(Vector3.Distance(centerPosition,  transform.position) > sphereRadius)
+		{
+			rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, (centerPosition - transform.position).normalized) * bounceRatio;
+			//Vector3 f = new Vector3(Random.value, Random.value, Random.value) * 20;
+			//rigidbody.AddForce( f);
+		}
+	}
+}
