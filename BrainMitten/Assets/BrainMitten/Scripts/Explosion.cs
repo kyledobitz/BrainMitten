1adaa0754219b2eadd011b96ac126b4529e3f3e9
kydo986 <kyledobitz@gmail.com>
2014-04-26 14:16:15 -0700

    Explosion plane example

---
+++ BrainMitten/Assets/BrainMitten/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	
+	public float radius = 5.0F;
+	public float power = 100.0F;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			if (hit && hit.rigidbody)
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, 3.0F);
+			
+		}
+	
+	}
+}
