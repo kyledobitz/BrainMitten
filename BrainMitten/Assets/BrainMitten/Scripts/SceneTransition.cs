aea97d6283e2ce2928d07bde7ba51ce4d1b413b8
kydo986 <kyledobitz@gmail.com>
2014-04-26 21:57:17 -0700

    Metal detector

---
+++ BrainMitten/Assets/BrainMitten/Scripts/SceneTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour {
+		
+	public string nextScene = "Credits Scene";
+	
+	void OnTriggerEnter(Collider collider){
+		if(collider.gameObject.tag == "Player")
+			Application.LoadLevel (nextScene);
+		
+	}
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "Player")
+			Application.LoadLevel (nextScene);
+		
+	}
+}
