16e5cc07748bb6d844eeb493a542b320c19740bc
David Selassie <selassid@gmail.com>
2014-04-26 21:07:20 -0700

    Adds in shadow scene and bead game scene. Probably messes everything else up.

---
+++ BrainMitten/Assets/BrainMitten/Scripts/WinningLightZoneScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinningLightZoneScript : MonoBehaviour {
+
+	public string nextScene;
+	public AudioClip winningSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			Debug.Log("Danger Zone!");
+			StartCoroutine(OnWin());
+		}
+	}
+
+	private IEnumerator OnWin () {
+		audio.PlayOneShot(winningSound);
+		yield return new WaitForSeconds(3);
+		if (!nextScene.Equals("")) {
+			Application.LoadLevel(nextScene);
+		}
+	}
+}
