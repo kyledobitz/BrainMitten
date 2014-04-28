9b3edcf5b3b815490a0045ad9e8ce24b120f29fa
kydo986 <kyledobitz@gmail.com>
2014-04-27 19:16:28 -0700

    With large Gojira scene

---
+++ BrainMitten/Assets/BrainMitten/Scripts/DestroyBuildingSounds.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBuildingSounds : MonoBehaviour {
+
+	public AudioClip[] destructionSounds;
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("Collision");
+		if (collision.gameObject.tag == "Destructible")
+			Debug.Log ("Destructible");
+			audio.PlayOneShot (destructionSounds [Random.Range (0, destructionSounds.Length)]);
+	}
+}
